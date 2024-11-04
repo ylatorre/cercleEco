323f0def660e3a8386a69e63579e01c797f8a5c0
enzodaloia <112956500+enzodaloia@users.noreply.github.com>
2024-11-04 12:56:35 +0100

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+  ],
   theme: {
     extend: {},
   },
