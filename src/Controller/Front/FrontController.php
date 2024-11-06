8f50aed0271ba4d5f3d4b880098c968420879e0d
enzodaloia <112956500+enzodaloia@users.noreply.github.com>
2024-11-06 14:53:15 +0100

    push push

--- src/Controller/Front/FrontController.php
+++ src/Controller/Front/FrontController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/front')]
 class FrontController extends AbstractController
 {
     #[Route('/', name: 'app_front')]
