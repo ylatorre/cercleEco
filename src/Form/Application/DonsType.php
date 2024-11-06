1fa2c1b49354793b2507856e313bc55cef0f862a
enzodaloia <112956500+enzodaloia@users.noreply.github.com>
2024-11-06 09:01:07 +0100

    ajout de dons

---
+++ src/Form/Application/DonsType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form\Application;
+
+use App\Entity\Application\Dons;
+use App\Entity\Application\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DonsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('image')
+            ->add('localisation')
+            ->add('categorie')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Dons::class,
+        ]);
+    }
+}
